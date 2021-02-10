fb54c8b193e11e08517b50da169f74d4819863b0
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-02-10 13:24:50 +0800

    Add files via upload

--- ClashByShell.c
+++ ClashByShell.c
@@ -3,18 +3,18 @@
 #include <io.h>
 
 FILE* profile,*SubList;
-int Run_Mode,Sub_Use,Sub_Num;
+int Main_Run_Mode,Sub_Run_Mode,Sub_Use,Sub_Num;
 char FileName[20],Command[160],SubURL[100],FileAddr[18];
-char UserAgent[23]="ClashforWindows/0.14.4";
+char UserAgent[19]="ClashByShell/0.4.0";
 
 int UI() {
-	if (!_access("log\\clash.health", 0)) {
+	if (!_access("working\\using.yaml", 0)) {
 		printf("--------------------------------------------\n");
 		printf("检测到Clash核心在后台运行，请不要关闭本窗口!\n");
 		printf("--------------------------------------------\n\n");
 	}
 	printf("请选择要执行的操作:\n\n1.启动Clash并设置系统代理\n\n2.打开控制面板\n\n3.更新Clash订阅\n\n4.解除UWP应用本地回环限制\n\n5.使用说明\n\n0.退出并关闭Clash核心\n\n请输入:");
-	scanf("%d", &Run_Mode);
+	scanf("%d", &Main_Run_Mode);
 	return 0;
 }
 
@@ -32,6 +32,10 @@ int Check() {
 	if (_access("bin", 0)) {
 		system("mkdir bin");
 	}
+	if (_access("working", 0)) {
+		system("mkdir working");
+	}
+	printf("正在检测Clash核心状态. . .\n");
 	if (_access("bin\\clash.exe", 6)) {
 		printf("----------------------------------------------------------------\n");
 		printf("检测不到Clash核心，请将clash核心命名为clash.exe并放到bin文件夹中\n");
@@ -40,7 +44,18 @@ int Check() {
 		system("pause > nul");
 		exit(0);
 	}
-	system("start /b nginx\\nginx.exe -p nginx");
+	else {
+		printf("已经检测到Clash核心!\n");
+		printf("正在检测国家级ip数据库. . .\n");
+		if (_access("working\\Country.mmdb", 0)) {
+			printf("正在下载国家级ip数据库. . .\n");
+			system("echo port: 7890> working\\config.yaml");
+			system("bin\\clash -d working -t working\\config.yaml");
+		}
+		printf("正在启动nginx. . .\n");
+		system("start /b nginx\\nginx.exe -p nginx");
+		system("cls");
+	}
 	return 0;
 }
 
@@ -64,26 +79,62 @@ int Sub_Update() {
 		system(Command);
 	}
 	else {
-		printf("检测到保存的订阅地址,列出所有订阅:\n");
+	Sub_Menu:printf("检测到保存的订阅地址,列出所有订阅:\n");
 		printf("----------------------------------\n");
 		system("type config\\sublist.txt");
 		printf("----------------------------------\n");
 		printf("需要添加并更新新订阅请输入0\n");
-		printf("请选择需要更新的订阅，输入序号即可:");
+		printf("请选择需要操作的订阅，输入序号即可:");
 		scanf("%d", &Sub_Use);
 		system("cls");
 		if (Sub_Use != 0) {
-			printf("请在弹出页输入订阅地址. . .\n");
-			sprintf(Command, "notepad config\\%d.yaml", Sub_Use);
-			system(Command);
+			printf("请选择需要的操作:\n\n1.修改订阅地址并更新订阅\n\n2.重命名\n\n3.删除此订阅\n\n4.退出\n\n请输入:");
+			scanf("%d", &Sub_Run_Mode);
 			system("cls");
-			sprintf(FileAddr, "config\\%d.yaml", Sub_Use);
-			profile = fopen(FileAddr, "r");
-			fscanf(profile, "%s", SubURL);
-			fclose(profile);
-			printf("正在更新订阅. . .\n");
-			sprintf(Command, "bin\\curl.exe -A \"%s\" \"%s\" > profile\\%d.yaml", UserAgent, SubURL,Sub_Use);
-			system(Command);
+			if (Sub_Run_Mode == 1) {
+				printf("请在弹出页输入订阅地址. . .\n");
+				sprintf(Command, "notepad config\\%d.yaml", Sub_Use);
+				system(Command);
+				system("cls");
+				sprintf(FileAddr, "config\\%d.yaml", Sub_Use);
+				profile = fopen(FileAddr, "r");
+				fscanf(profile, "%s", SubURL);
+				fclose(profile);
+				printf("正在更新订阅. . .\n");
+				sprintf(Command, "bin\\curl.exe -A \"%s\" \"%s\" > profile\\%d.yaml", UserAgent, SubURL, Sub_Use);
+				system(Command);
+			}
+			else if (Sub_Run_Mode == 2) {
+				printf("订阅保存的文件名:");
+				scanf("%s", FileName);
+				sprintf(Command, "type config\\sublist.txt | find /v \"%d.\" > config\\sublist.txt_temp", Sub_Use);
+				system(Command);
+				sprintf(Command, "echo %d.%s >> config\\sublist.txt_temp",Sub_Use,FileName);
+				system(Command);
+				sprintf(Command, "copy /y config\\sublist.txt_temp config\\sublist.txt");
+				system(Command);
+				sprintf(Command, "del /F /S /Q config\\sublist.txt_temp");
+				system(Command);
+				system("cls");
+				goto Sub_Menu;
+			}
+			else if (Sub_Run_Mode == 3) {
+				sprintf(Command,"del /F /S /Q profile\\%d.yaml",Sub_Use);
+				system(Command);
+				sprintf(Command, "del /F /S /Q config\\%d.yaml", Sub_Use);
+				system(Command);
+				sprintf(Command, "type config\\sublist.txt | find /v \"%d.\" > config\\sublist.txt_temp", Sub_Use);
+				system(Command);
+				sprintf(Command, "copy /y config\\sublist.txt_temp config\\sublist.txt");
+				system(Command);
+				sprintf(Command, "del /F /S /Q config\\sublist.txt_temp");
+				system(Command);
+				system("cls");
+				goto Sub_Menu;
+			}
+			else {
+				exit(0);
+			}
 		}
 		else {
 			for (i = 1; i <= 200; i++) {
@@ -120,9 +171,9 @@ int Sub_Update() {
 
 int main() {
 	Check();
-MENU:UI();
+Main_Menu:UI();
 	system("cls");
-	if (Run_Mode == 1) {
+	if (Main_Run_Mode == 1) {
 		if (_access("profile\\1.yaml", 0)) {
 			printf("检测到从未指定过配置文件，请先更新订阅!\n");
 			system("pause");
@@ -138,9 +189,10 @@ MENU:UI();
 			scanf("%d", &Sub_Use);
 			system("cls");
 			system("taskkill /f /im clash.exe");
-			sprintf(Command, "start /b bin\\clash.exe -d profile -f profile\\%d.yaml >> log\\log.txt",Sub_Use);
+			sprintf(Command, "copy /y profile\\%d.yaml working\\using.yaml", Sub_Use);
 			system(Command);
-			system("echo 1 > log\\clash.health");
+			sprintf(Command, "start /b bin\\clash.exe -d working -f working\\using.yaml > log\\log.txt");
+			system(Command);
 			system("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 1 /f");
 			system("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /d \"http://127.0.0.1:7890;socks://127.0.0.1:7891\" /f");
 			system("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyOverride /t REG_SZ /d \"localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*;<local>\" /f");
@@ -150,31 +202,31 @@ MENU:UI();
 			system("explorer http://127.0.0.1:8080/#/proxies");
 		}
 	}
-	else if (Run_Mode == 2) {
+	else if (Main_Run_Mode == 2) {
 		printf("正在打开控制台. . .\n");
 		system("explorer http://127.0.0.1:8080/#/proxies");
 	}
-	else if (Run_Mode == 3) {
+	else if (Main_Run_Mode == 3) {
 		Sub_Update();
 	}
-	else if (Run_Mode == 4) {
+	else if (Main_Run_Mode == 4) {
 		system("bin\\EnableLoopback.exe");
 	}
-	else if (Run_Mode == 5) {
-		printf("首次使用请先更新订阅,调整节点请使用控制台!\n");
-		system("pause");
+	else if (Main_Run_Mode == 5) {
+		printf("正在打开帮助. . .\n");
+		system("explorer https://hxhgts.ml/ClashByShell/");
 	}
-	else if (Run_Mode == 0) {
+	else if (Main_Run_Mode == 0) {
 		printf("正在退出. . .\n");
 		system("taskkill /f /im clash.exe");
 		system("taskkill /f /im nginx.exe");
-		system("del log\\clash.health");
 		system("del /F /S /Q nginx\\logs\\*");
+		system("del /F /S /Q working\\using.yaml");
 		system("reg add \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 00000000 /f");
 		system("reg add \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /t REG_DWORD /d 00000000 /f");
 		system("reg add \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyOverride /t REG_DWORD /d 00000000 /f");
 		exit(0);
 	}
 	system("cls");
-	goto MENU;
+	goto Main_Menu;
 }
